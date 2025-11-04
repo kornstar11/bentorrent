fc0543de93b6b8b8b0a6a3780c11d24879b3022c
Ben Kornmeier <benkornmeier@gmail.com>
2025-11-04 11:07:13 -0500

    to_bytes

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,17 @@ use std::fs::read;
 
 fn main() {
     let torrent = read("/Users/bkornmeier/Downloads/ubuntu-25.10-desktop-amd64.iso.torrent").unwrap();
-    let (_, parsed) = bentorrent::parse_bencode(&torrent).unwrap();
+    match bentorrent::parse_bencode(&torrent) {
+        Ok((_, parsed)) => {
+            println!("{:#?}", parsed);
+        },
+        Err(nom::Err::Error(e)) => {
+            eprintln!("Error: {:?}", e);
+        }
+        Err(nom::Err::Failure(e)) => {
+            eprintln!("Fail: {:?}", e);
+        }
+    }
 
-    println!("{:#?}", parsed);
 
 }
