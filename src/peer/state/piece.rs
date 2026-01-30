0e066e5a86a85778646d936f2d5b1e2f9d3e90c1
Ben Kornmeier <benkornmeier@gmail.com>
2026-01-30 09:03:23 -0500

    cleanup

--- src/peer/state/piece.rs
+++ src/peer/state/piece.rs
@@ -350,12 +350,6 @@ impl PieceBlockTracker {
         torrent: &V1Torrent,
         availiable_piece_to_peers: &BTreeMap<u32, HashSet<InternalPeerId>>,
     ) -> Vec<PeerRequestedPiece> {
-        // TODO we need a nice way to decide what pieces we should get next.
-        // let sorted = availiable_piece_to_peers
-        //     .clone()
-        //     .into_iter()
-        //     .collect::<BTreeMap<u32, HashSet<InternalPeerId>>>();
-
         availiable_piece_to_peers
             .iter()
             .flat_map(|(piece_id, peers_with_piece)| {
