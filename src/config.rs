3f5eb8b698ba4dcf37f716af559712fd035d1bc4
Ben <kornstar11@users.noreply.github.com>
2026-02-04 22:13:17 -0500

    cli (#8)

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,30 @@
-#[derive(Debug, Clone)]
+use clap::Args;
+
+#[derive(Args, Debug, Clone)]
+#[group(required = false, multiple = false)]
 pub struct Config {
+    #[arg(default_value_t = true)]
+    #[arg(long)]
     pub use_file_writer: bool,
+    #[arg(default_value_t = 4)]
+    #[arg(long)]
     pub max_conns: usize,
+    #[arg(default_value_t = 128)]
+    #[arg(long)]
     pub peer_tx_size: usize,
+    #[arg(default_value_t = 128)]
+    #[arg(long)]
     pub peer_rx_size: usize,
+    #[arg(default_value_t = 2)]
+    #[arg(long)]
     pub file_handler_channel_size: usize,
+    #[arg(default_value_t = 128)]
+    #[arg(long)]
     pub max_outstanding_requests: usize,
 }
 
-impl Config {
-    pub fn new() -> Self {
+impl Default for Config {
+    fn default() -> Self {
         Self {
             use_file_writer: true,
             max_conns: 4,
